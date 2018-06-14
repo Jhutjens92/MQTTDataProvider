4e9938480c26b080a75b393c2acfcf31abc1c99e
jhutjens92 <jordi.hutjens@home.nl>
2018-06-14 15:58:25 +0200

    Fixed not sending to Learning Hub
    - Removed obsolete code

--- MainWindowView.xaml.cs
+++ MainWindowView.xaml.cs
@@ -43,7 +43,6 @@ namespace MQTTDataProvider
         public static bool debug = false;           //debug value for enable debugging
 
         MQTTManager.MQTTManager MQTTManager = new MQTTManager.MQTTManager();
-        MQTTManager.LHConnector LHConnector = new MQTTManager.LHConnector();
 
         // this code runs when the main window opens (start of the app)
         public MainWindowView()
@@ -55,10 +54,10 @@ namespace MQTTDataProvider
 
             //INIT Functions//
             InitializeComponent();
-            LHConnector.SetValueNames();
+            MQTTManager.SetValueNames();
             
-            MQTTDataProvider.MQTTManager.LHConnector.myConnector.stopRecordingEvent += MyConnector_stopRecordingEvent;
-            MQTTDataProvider.MQTTManager.LHConnector.myConnector.startRecordingEvent += MyConnector_startRecordingEvent;
+            MQTTDataProvider.MQTTManager.MQTTManager.myConnector.stopRecordingEvent += MyConnector_stopRecordingEvent;
+            MQTTDataProvider.MQTTManager.MQTTManager.myConnector.startRecordingEvent += MyConnector_startRecordingEvent;
 
             //MQTT Functions//
             client = new MqttClient(BrokerAddress);
@@ -123,11 +122,11 @@ namespace MQTTDataProvider
             string SHT1X1_Hum_Value = Parsed_ReceivedMessage.shts[0].hum;
             client.Publish("wekit/vest/Sht0_Hum", Encoding.UTF8.GetBytes(SHT1X1_Hum_Value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
 
-            string SHT1X2_Temp_Value = Parsed_ReceivedMessage.shts[1].temp;
-            client.Publish("wekit/vest/Sht1_Temp", Encoding.UTF8.GetBytes(SHT1X2_Temp_Value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            string SHT2X2_Temp_Value = Parsed_ReceivedMessage.shts[1].temp;
+            client.Publish("wekit/vest/Sht2_Temp", Encoding.UTF8.GetBytes(SHT2X2_Temp_Value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
 
-            string SHT1X2_Hum_Value = Parsed_ReceivedMessage.shts[1].hum;
-            client.Publish("wekit/vest/Sht1_Hum", Encoding.UTF8.GetBytes(SHT1X2_Hum_Value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+            string SHT2X2_Hum_Value = Parsed_ReceivedMessage.shts[1].hum;
+            client.Publish("wekit/vest/Sht2_Hum", Encoding.UTF8.GetBytes(SHT2X2_Hum_Value), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
         }
 
         // this code runs when a message was received
@@ -143,6 +142,7 @@ namespace MQTTDataProvider
 
                 JSONParse_ReceivedMessage();
                 Publish_Data();
+                MQTTManager.MQTT_DataAcquired();
             }
         }
 
@@ -166,18 +166,31 @@ namespace MQTTDataProvider
                          () =>
                          {
                              IMU1_AccX.Text = Parsed_ReceivedMessage.imus[0].ax;
+                             MQTTManager.IMU1_AccX = IMU1_AccX.Text;
                              IMU1_AccY.Text = Parsed_ReceivedMessage.imus[0].ay;
+                             MQTTManager.IMU1_AccY = IMU1_AccY.Text;
                              IMU1_AccZ.Text = Parsed_ReceivedMessage.imus[0].az;
+                             MQTTManager.IMU1_AccZ = IMU1_AccZ.Text;
                              IMU1_GyroX.Text = Parsed_ReceivedMessage.imus[0].gx;
+                             MQTTManager.IMU1_GyroX = IMU1_GyroX.Text;
                              IMU1_GyroY.Text = Parsed_ReceivedMessage.imus[0].gy;
+                             MQTTManager.IMU1_GyroY = IMU1_GyroY.Text;
                              IMU1_GyroZ.Text = Parsed_ReceivedMessage.imus[0].gz;
+                             MQTTManager.IMU1_GyroZ = IMU1_GyroZ.Text;
                              IMU1_MagX.Text = Parsed_ReceivedMessage.imus[0].mx;
+                             MQTTManager.IMU1_MagX = IMU1_MagX.Text;
                              IMU1_MagY.Text = Parsed_ReceivedMessage.imus[0].my;
+                             MQTTManager.IMU1_MagY = IMU1_MagY.Text;
                              IMU1_MagZ.Text = Parsed_ReceivedMessage.imus[0].mz;
+                             MQTTManager.IMU1_MagZ = IMU1_MagZ.Text;
                              IMU1_Q0.Text = Parsed_ReceivedMessage.imus[0].q0;
+                             MQTTManager.IMU1_Q0 = IMU1_Q0.Text;
                              IMU1_Q1.Text = Parsed_ReceivedMessage.imus[0].q1;
+                             MQTTManager.IMU1_Q1 = IMU1_Q1.Text;
                              IMU1_Q2.Text = Parsed_ReceivedMessage.imus[0].q2;
+                             MQTTManager.IMU1_Q2 = IMU1_Q2.Text;
                              IMU1_Q3.Text = Parsed_ReceivedMessage.imus[0].q3;
+                             MQTTManager.IMU1_Q3 = IMU1_Q3.Text;
 
                          }));
         }
@@ -187,18 +200,31 @@ namespace MQTTDataProvider
                          () =>
                          {
                              IMU2_AccX.Text = Parsed_ReceivedMessage.imus[1].ax;
+                             MQTTManager.IMU2_AccX = IMU2_AccX.Text;
                              IMU2_AccY.Text = Parsed_ReceivedMessage.imus[1].ay;
+                             MQTTManager.IMU2_AccY = IMU2_AccY.Text;
                              IMU2_AccZ.Text = Parsed_ReceivedMessage.imus[1].az;
+                             MQTTManager.IMU2_AccZ = IMU2_AccZ.Text;
                              IMU2_GyroX.Text = Parsed_ReceivedMessage.imus[1].gx;
+                             MQTTManager.IMU2_GyroX = IMU2_GyroX.Text;
                              IMU2_GyroY.Text = Parsed_ReceivedMessage.imus[1].gy;
+                             MQTTManager.IMU2_GyroY = IMU2_GyroY.Text;
                              IMU2_GyroZ.Text = Parsed_ReceivedMessage.imus[1].gz;
+                             MQTTManager.IMU2_GyroZ = IMU2_GyroZ.Text;
                              IMU2_MagX.Text = Parsed_ReceivedMessage.imus[1].mx;
+                             MQTTManager.IMU2_MagX = IMU2_MagX.Text;
                              IMU2_MagY.Text = Parsed_ReceivedMessage.imus[1].my;
+                             MQTTManager.IMU2_MagY = IMU2_MagY.Text;
                              IMU2_MagZ.Text = Parsed_ReceivedMessage.imus[1].mz;
+                             MQTTManager.IMU2_MagZ = IMU2_MagZ.Text;
                              IMU2_Q0.Text = Parsed_ReceivedMessage.imus[1].q0;
+                             MQTTManager.IMU2_Q0 = IMU2_Q0.Text;
                              IMU2_Q1.Text = Parsed_ReceivedMessage.imus[1].q1;
+                             MQTTManager.IMU2_Q1 = IMU2_Q1.Text;
                              IMU2_Q2.Text = Parsed_ReceivedMessage.imus[1].q2;
+                             MQTTManager.IMU2_Q2 = IMU2_Q2.Text;
                              IMU2_Q3.Text = Parsed_ReceivedMessage.imus[1].q3;
+                             MQTTManager.IMU2_Q3 = IMU2_Q3.Text;
                          }));
         }
 
@@ -208,7 +234,9 @@ namespace MQTTDataProvider
                          () =>
                          {
                              SHT1X1_Temp.Text = Parsed_ReceivedMessage.shts[0].temp;
+                             MQTTManager.Temp_External = SHT1X1_Temp.Text;
                              SHT1X1_Hum.Text = Parsed_ReceivedMessage.shts[0].hum;
+                             MQTTManager.Humidity_External = SHT1X1_Hum.Text;
                          }));
         }
 
@@ -217,8 +245,10 @@ namespace MQTTDataProvider
             Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
                          () =>
                          {
-                             SHT1X2_Temp.Text = Parsed_ReceivedMessage.shts[1].temp;
-                             SHT1X2_Hum.Text = Parsed_ReceivedMessage.shts[1].hum;
+                             SHT1X2_Temp.Text = Parsed_ReceivedMessage.shts[0].temp;
+                             MQTTManager.Temp_Internal = SHT1X2_Temp.Text;
+                             SHT1X2_Hum.Text = Parsed_ReceivedMessage.shts[0].hum;
+                             MQTTManager.Humidity_Internal = SHT1X2_Hum.Text;
                          }));
         }
 
@@ -228,6 +258,7 @@ namespace MQTTDataProvider
                          () =>
                          {
                              Pulse_TempLobe.Text = Parsed_ReceivedMessage.pulse;
+                             MQTTManager.Pulse_TempLobe = Pulse_TempLobe.Text;
                          }));
         }
 
@@ -237,6 +268,7 @@ namespace MQTTDataProvider
                          () =>
                          {
                              GSR.Text = Parsed_ReceivedMessage.gsr;
+                             MQTTManager.GSR = GSR.Text;
                          }));
         }
 
